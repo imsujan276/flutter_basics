8cc3b0afa41185128b77a63a64264ef632e613f3
Shashi Sharma <sharmashashi940@gmail.com>
2019-12-05 10:01:41 +0545

    implemented tab bar view

--- lib/facebook_ui/facebook.dart
+++ lib/facebook_ui/facebook.dart
@@ -7,31 +7,68 @@ class Facebook extends StatefulWidget {
 }
 
 class _FacebookState extends State<Facebook> {
+  List<Widget> _tabsBody = [
+    ListView(
+      children: <Widget>[
+        NewsFeedCard('Jenny Cruise'),
+        NewsFeedCard('Emma Clark')
+      ],
+    ),
+    Text('this is second tab'),
+    Text('this is third tab'),
+    Text('this is fourth tab'),
+    Text('this is fifth tab'),
+    Text('this is sixth tab'),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(),
-      backgroundColor: Colors.grey[300],
-      appBar: AppBar(
-        //for messenger icon
-        actions: <Widget>[
-          actionButton(assetImage: AssetImage('assets/search.png')),
-          actionButton(assetImage: AssetImage('assets/messenger.png')),
-        ],
-        backgroundColor: Colors.white,
-        title: ImageIcon(
-          AssetImage('assets/facebook_logo.png'),
-          color: Colors.blue,
-          size: 120,
-        ),
-      ),
-      body: ListView(
-        children: <Widget>[
-          NewsFeedCard('Jenny Cruise'),
-          NewsFeedCard('Emma Clark')
-        ],
-      ),
-    );
+    return DefaultTabController(
+        initialIndex: 0,
+        length: 6,
+        child: Scaffold(
+            drawer: Drawer(),
+            backgroundColor: Colors.grey[300],
+            appBar: AppBar(
+              bottom: TabBar(
+                tabs: <Widget>[
+                  Icon(
+                    Icons.home,
+                    color: Colors.black,
+                  ),
+                  Icon(
+                    Icons.group,
+                    color: Colors.black,
+                  ),
+                  Icon(
+                    Icons.live_tv,
+                    color: Colors.black,
+                  ),
+                  Icon(
+                    Icons.account_circle,
+                    color: Colors.black,
+                  ),
+                  Icon(Icons.notifications, color: Colors.black),
+                  Icon(
+                    Icons.menu,
+                    color: Colors.black,
+                  )
+                ],
+              ),
+              //for messenger icon
+              actions: <Widget>[
+                actionButton(assetImage: AssetImage('assets/search.png')),
+                actionButton(assetImage: AssetImage('assets/messenger.png')),
+              ],
+              backgroundColor: Colors.white,
+              title: ImageIcon(
+                AssetImage('assets/facebook_logo.png'),
+                color: Colors.blue,
+                size: 120,
+              ),
+            ),
+            body: TabBarView(
+              children: _tabsBody,
+            )));
   }
 
   ///returns action buttons in app bar
